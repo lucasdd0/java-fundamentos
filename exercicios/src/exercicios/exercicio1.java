96b861449ee251e292e0ffb49a16fffc5f80237b
lucasdd0 <lucasfo2005@gmail.com>
2024-04-11 18:40:26 -0300

    commit

---
+++ exercicios/src/exercicios/exercicio1.java
@@ -0,0 +1,16 @@
+package exercicios;
+import java.util.Scanner;
+public class exercicio1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+		
+		input.close();
+	}
+
+}
